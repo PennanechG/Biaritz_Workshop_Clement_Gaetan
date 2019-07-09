d621f48f5612e0aa637ac4326db57484eb4a00dc
zepolclem <lopez.clmnt@gmail.com>
2019-07-09 10:03:57 +0200

    varables check

--- revisions/index.php
+++ revisions/index.php
@@ -1,6 +1,26 @@
 <?php
 
 echo "Hello fellowship";
+echo '<br>';
+
+$ringBearer = "Frodon";
+echo $ringBearer;
+echo '<br>';
+$ringMaker = "Sauron";
+echo $ringMaker;
+echo '<br>';
+
+$ringCompagnyMembers = 9;
+
+$ringOnFinger = true;
+
+$ringBearer = "Sam";
+
+$ringCompagnyMembers += 1;
+
+echo $ringCompagnyMembers;
+echo '<br>';
+$middleHearth ="";
+echo $middleHearth;
 
 ?>
-
